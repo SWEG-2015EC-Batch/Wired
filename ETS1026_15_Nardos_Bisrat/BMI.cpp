a8b208d86135b7e9063946c49d17513672391155
Nardos Bisrat <149002589+nardi-bis@users.noreply.github.com>
2023-11-17 10:30:05 -0800

    Update BMI.cpp

--- ETS1026_15_Nardos_Bisrat/BMI.cpp
+++ ETS1026_15_Nardos_Bisrat/BMI.cpp
@@ -1,1 +1,19 @@
+//calculate the BMI
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float weight;
+    float height;
+    cout <<"please enter weight"<< endl;
+    cin>>weight;
+    cout <<"please enter Height"<< endl;
+    cin>>height;
+    float BMI=weight/(height*height);
+    cout<<"the BMI is"<<BMI<< endl;
+
+	   return 0;
+}
 
