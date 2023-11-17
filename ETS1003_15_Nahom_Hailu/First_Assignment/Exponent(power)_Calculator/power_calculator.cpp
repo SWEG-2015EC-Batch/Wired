728315d9f529b7f6ff6e72e5a0df03a928946c8e
NahomHailu12 <148992627+NahomHailu12@users.noreply.github.com>
2023-11-17 20:38:59 +0300

    Create power_calculator.cpp

---
+++ ETS1003_15_Nahom_Hailu/First_Assignment/Exponent(power)_Calculator/power_calculator.cpp
@@ -0,0 +1,12 @@
+   // program to run Exponents//
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    double base, power;
+cout << "Wellcome this system returns first number power of second number"<< endl;
+    cout << "enter the first number ";
+    cin >> base;
+    cout << "enter the secound number ";
+    cin >> power;
+cout << "the result is equals to " << pow(base,power); 
