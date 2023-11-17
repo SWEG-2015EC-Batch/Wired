affcadd93d75f4898790e2f39199cda2814fec06
NahomHailu12 <148992627+NahomHailu12@users.noreply.github.com>
2023-11-17 19:10:53 +0300

    Create Automobile_driven_Test.cpp

---
+++ ETS1003_15_Nahom_Hailu/First_Assignment/Automobile_driven_Test/Automobile_driven_Test.cpp
@@ -0,0 +1,14 @@
+ // Program to caluclate maximum number of mile car can go //
+#include <iostream>
+using namespace std;
+int main() {
+    float capacityOfTank(0); // capacity of fuel to hold in tank in gallon//
+    float milePerGallon(0); // amount of car can go in mile per gallon//
+    cout << "Enter the capacity in gallon the capacity of fuel tank" << endl;
+    cin >> capacityOfTank;
+    cout << " Enter the miles the car could be driven per gallon" << endl;
+    cin >> milePerGallon; 
+    float  totalDrive = capacityOfTank * milePerGallon;
+    cout << "Therefore the car can go " << totalDrive << " miles.";
+    return 0;
+}
