c07b9fe8a89d2ff1e4350bfea977937f6f88448b
Nardos Bisrat <149002589+nardi-bis@users.noreply.github.com>
2023-11-17 10:27:11 -0800

    Update Exponential.cpp

--- ETS1026_15_Nardos_Bisrat/Exponential.cpp
+++ ETS1026_15_Nardos_Bisrat/Exponential.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <cmath>
+#include <ctype.h>
+
+using namespace std;
+
+int main()
+{
+  int x,y;
+  int result;
+
+  cout << "Enter the value of X: ";
+  cin >> x;
+
+  cout << "Enter the value of Y: ";
+  cin >> y;
+
+  result = pow(x, y);
+
+  cout << "The result of X^Y is: " << result;
+
+  return 0;
+}
 
