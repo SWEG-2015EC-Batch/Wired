1e2c542d2aef465b4e1a1b615455cdaa54a7cfe2
Nardos Bisrat <149002589+nardi-bis@users.noreply.github.com>
2023-11-17 12:17:11 -0800

    Update Miles.cpp

--- ETS1026_15_Nardos_Bisrat/Miles.cpp
+++ ETS1026_15_Nardos_Bisrat/Miles.cpp
@@ -1,1 +1,15 @@
-
+//c++ program that calculate the number of miles that an automobile can be driven without refueling
+#include <iostream>
+using namespace std;
+int main()
+{
+  float fuelcapacity;
+  float mpg;//mpg is mile per gallon
+  cout << "Enter the capacity of the fuel tank in gallons";
+  cin >> fuelcapacity;
+  cout << "Enter mpg";
+  cin >> mpg;
+  float miles=fuelcapacity*mpg;
+  cout << "You can go " << miles << " miles with full gallon";
+  return 0;
+}
