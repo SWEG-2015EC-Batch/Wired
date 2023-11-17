34af8c5e3b754f591e9aede2ef3fe47f899cc90e
Nardos Bisrat <149002589+nardi-bis@users.noreply.github.com>
2023-11-17 12:52:48 -0800

    Update byte.cpp

--- ETS1026_15_Nardos_Bisrat/byte.cpp
+++ ETS1026_15_Nardos_Bisrat/byte.cpp
@@ -1,1 +1,14 @@
-
+//c++ program that calculates the time taken to send a file in bytes
+#include <iostream>
+using namespace std;
+int main()
+{
+const int transmissionRate = 960;
+   int fileSizeInBytes;
+    int timeInSec;
+  cout << "Enter the file size in bytes";
+  cin >> fileSizeInBytes;
+timeInSec= fileSizeInBytes / transmissionRate;
+cout << "time needed to send the file " << timeInSec<< "sec" << endl;
+return 0;
+}
