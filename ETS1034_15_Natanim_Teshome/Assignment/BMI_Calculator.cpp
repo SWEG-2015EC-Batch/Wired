16b5dc974bf225e7c0603bbeebf99713abb4b9b5
Natanim Teshome <118844475+NatanimT@users.noreply.github.com>
2023-11-18 11:38:39 +0300

    Update BMI_Calculator.cpp

--- ETS1034_15_Natanim_Teshome/Assignment/BMI_Calculator.cpp
+++ ETS1034_15_Natanim_Teshome/Assignment/BMI_Calculator.cpp
@@ -1,1 +1,22 @@
+// calculate BMI
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+  // declaring variable
+    float weight;
+    float height;
+  //promote user to enter weight
+    cout <<"please enter weight"<< endl;
+    cin>>weight;
+  // promot user to enter height
+    cout <<"please enter Height"<< endl;
+    cin>>height;
+    float BMI=weight/(height*height);
+  //print BMI
+    cout<<"the BMI is"<<BMI<< endl;
+
+	   return 0;
+}
