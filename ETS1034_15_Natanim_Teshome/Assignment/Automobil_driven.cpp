055666db29a2e8ef590b3033648681d1da033ffe
Natanim Teshome <118844475+NatanimT@users.noreply.github.com>
2023-11-18 10:55:59 +0300

    Update Automobil_driven.cpp

--- ETS1034_15_Natanim_Teshome/Assignment/Automobil_driven.cpp
+++ ETS1034_15_Natanim_Teshome/Assignment/Automobil_driven.cpp
@@ -1,1 +1,17 @@
+  //the number miles automobil can be driven without refueling
+#include <iostream>
+using namespace std;
+int main(){
+  //declearing variables
+   int fuleTank;
+   int milePerGallons;
+  //promots the user to enter capacity of fule tank
+   cout<<"Please Enter the capacity of fule tank in gallons" <<endl;
+   cin>>fuleTank;
+  //promots the user to enter mile
+   cout<<"Please Enter the mile per gallon the automobile can be driven" <<endl;
+   cin>>milePerGallons;
+   cout<<"Automobile can be driven without refuiling"  << fuleTank * milePerGallons <<endl;
+    return 0;
+}
 
