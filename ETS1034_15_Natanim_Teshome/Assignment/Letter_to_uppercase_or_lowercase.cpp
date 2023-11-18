051596fbdeaf7d35f87ce11a7cbd50e453340cfc
Natanim Teshome <118844475+NatanimT@users.noreply.github.com>
2023-11-18 11:29:48 +0300

    Update Letter_to_uppercase_or_lowercase.cpp

--- ETS1034_15_Natanim_Teshome/Assignment/Letter_to_uppercase_or_lowercase.cpp
+++ ETS1034_15_Natanim_Teshome/Assignment/Letter_to_uppercase_or_lowercase.cpp
@@ -1,1 +1,29 @@
-
+// c++ program that convert upper and lower case letters to their equivalent
+#include <iostream>
+#include <ctype.h> // usefule library
+using namespace std;
+int main()
+{
+    //declaring variable
+    char letter;
+    //input the letter
+    cout<<"enter letter";
+    cin>>letter;
+    //check if it is lowercase
+    if (islower(letter))
+        {
+        // convert to uppercase
+        letter = toupper(letter);
+        cout<<"Uppercase equivalent"<<letter<<endl;
+    }
+    else if (isupper(letter))
+        {
+        // convert to lowercase
+        letter = tolower(letter);
+        cout<<"Lowercase equivalent"<<letter<<endl;
+    }
+    else {
+        cout<<"Invalid input. Please enter a valid letter "<<endl;
+    }
+    return 0;
+}
