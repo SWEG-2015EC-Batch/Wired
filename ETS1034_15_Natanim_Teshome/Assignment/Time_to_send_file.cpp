563f387e63a100412113bc0c00bb01fbb711f021
Natanim Teshome <118844475+NatanimT@users.noreply.github.com>
2023-11-18 11:21:16 +0300

    Update Time_to_send_file.cpp

--- ETS1034_15_Natanim_Teshome/Assignment/Time_to_send_file.cpp
+++ ETS1034_15_Natanim_Teshome/Assignment/Time_to_send_file.cpp
@@ -1,1 +1,16 @@
-
+//program that calculates the time taken to send a file in bytes
+#include <iostream>
+using namespace std;
+int main()
+{
+const int transmissionRate = 960;
+// declering variable for file size
+   int fs;
+// declering variable for time in seconds
+    int tis;
+  cout << "please enter the file size in bytes " ;
+  cin >> fs;
+tis= fs / transmissionRate;
+cout << "the time needed to send the file " << tis<< "sec" << endl;
+return 0;
+}
