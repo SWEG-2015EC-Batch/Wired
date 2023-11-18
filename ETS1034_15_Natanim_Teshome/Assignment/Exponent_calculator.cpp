4ebfac05621af05efb0e2bb447546e92f4f00ad4
Natanim Teshome <118844475+NatanimT@users.noreply.github.com>
2023-11-18 11:14:57 +0300

    Update Exponent_calculator.cpp

--- ETS1034_15_Natanim_Teshome/Assignment/Exponent_calculator.cpp
+++ ETS1034_15_Natanim_Teshome/Assignment/Exponent_calculator.cpp
@@ -1,1 +1,26 @@
 
+//program that calculate x the power of y
+#include <iostream>
+#include <cmath> // library to do the math
+using namespace std;
+
+int main()
+{
+    // declaring variable
+  int x,y;
+  int result;
+//promote user to enter value X
+  cout << "please enter the value of X ";
+  cin >> x;
+//promote user to enter value Y
+  cout << "please enter the value of Y ";
+  cin >> y;
+
+  result = pow(x, y);
+  //print result
+  cout << "The result of X^Y is: " << result;
+
+  return 0;
+}
+   
+    
