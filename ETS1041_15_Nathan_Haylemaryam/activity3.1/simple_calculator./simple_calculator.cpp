49e9b120b271d7c769fd9e289cc937a2a3188af5
Nathan Haylemaryam <148970236+NathanHaylemaryam@users.noreply.github.com>
2023-12-15 23:42:03 +0300

    Create simple_calculator.cpp

---
+++ ETS1041_15_Nathan_Haylemaryam/activity3.1/simple_calculator./simple_calculator.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+int main() {
+    double NUM1,NUM2,result;
+    char operation;
+    
+
+    cout<<"enter the first number: ";
+    cin>>NUM1;
+   do { 
+    cout<<"enter the opereation you would like to perfrom:";
+    cin>>operation;
+ if (operation == '0') {
+            cout << "Exiting the program." << endl;
+            cout<<"your result is: "<<result<<endl;
+            break; // exit the loop
+        }
+    cout<<"enter the next number:";
+    cin>>NUM2;
+    
+    switch (operation) {
+        case '+':
+            result =NUM1+NUM2;
+            break;
+        case '-':
+            result = NUM1 - NUM2;
+            break;
+        case '*':
+            result = NUM1 * NUM2;
+            break;
+        case '/':
+            
+            if (NUM2 != 0) {
+                result = NUM1 / NUM2;
+            } else {
+                cout << "Error: Division by zero is undefined." << endl;
+                return 1; 
+            }
+            break;
+        default:
+            cout << "Error: Invalid operator." << endl;
+            return 1;
+    } 
+             cout << "Result: " << result << endl;
+             NUM1=result;
+    }while (operation!='0');
+    return 0;}
