76430f40b10f4cf1647ed7e3957398241e4a3397
Nathan Haylemaryam <148970236+NathanHaylemaryam@users.noreply.github.com>
2023-12-17 00:04:51 +0300

    Update sourcecode.cpp

--- ETS1041_15_Nathan_Haylemaryam/activity3.1/printing_pattern/capital_letter_triangle/sourcecode.cpp
+++ ETS1041_15_Nathan_Haylemaryam/activity3.1/printing_pattern/capital_letter_triangle/sourcecode.cpp
@@ -1,1 +1,14 @@
-
+#include <iostream>
+using namespace std;
+int main(){
+int side,j(1);
+cout << "enter the side: ";
+cin>> side;
+for(int i=1;i<=side;i++){
+    for(int k=1;k<=j;k++){
+        cout << char(64+k)<< "  ";
+    }
+    j++;
+    cout << endl;
+}
+}
