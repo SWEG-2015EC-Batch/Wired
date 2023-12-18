c80f9c3a95b5e6282c1c881b156076e042d55396
NahomHailu12 <148992627+NahomHailu12@users.noreply.github.com>
2023-12-18 11:13:41 +0300

    Update even_digit_sum.cpp

--- ETS1003_15_Nahom_Hailu/Activity_3.1/Playing_with_digits/even_digit_sum.cpp
+++ ETS1003_15_Nahom_Hailu/Activity_3.1/Playing_with_digits/even_digit_sum.cpp
@@ -2,15 +2,15 @@
 #include <iostream>
 using namespace std;
 int main() {
-int number, remainder, sum=0;
+int number, remainder, product=1;
 cout << "Enter the number: ";
 cin >> number;
 while(number > 0){
 remainder = number % 10;
 if(remainder%2==0) {
-    sum += remainder;
+    product *= remainder;
 }
 number = number / 10;
 }
-cout <<"The sum of even digit is " << sum;
+cout <<"The sum of even digit is " << product;
 }
