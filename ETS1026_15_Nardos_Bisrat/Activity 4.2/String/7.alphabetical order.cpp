2c3029e0de916c081ab467cd8a9b5b45cc73a190
Nardos Bisrat <149002589+nardi-bis@users.noreply.github.com>
2024-01-14 23:08:08 +0300

    Update 7.alphabetical order.cpp

--- ETS1026_15_Nardos_Bisrat/Activity 4.2/String/7.alphabetical order.cpp
+++ ETS1026_15_Nardos_Bisrat/Activity 4.2/String/7.alphabetical order.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+
+    int size = 5;
+    string names[size];
+
+    for (int i = 0; i < size; i++){
+        cout << "Enter name #" << i+1 << ": ";
+        getline(cin, names[i]);
+    }
+    
+    for(int i = 0; i < size -1; i++){
+        for(int j = 0; j < size - i - 1; j++){
+            string temp;
+            if(names[j][0] > names[j +1][0]){
+                temp = names[j];
+                names[j] = names[j+1];
+                names[j+1] = temp;
+            }
+        }
+    }
+
+    cout << "The names in alphabetical order:-\n";
+    for (int i = 0; i < size; i++)
+    {
+        cout << names[i] << '\n';
+    }
+
+    return 0;
+}
