e68e6ec4e3953e7604c39b13c7e104ad95663f99
Nardos Bisrat <149002589+nardi-bis@users.noreply.github.com>
2024-01-14 22:48:40 +0300

    Update 11.generating user initial.cpp

--- ETS1026_15_Nardos_Bisrat/Activity 4.2/String/11.generating user initial.cpp
+++ ETS1026_15_Nardos_Bisrat/Activity 4.2/String/11.generating user initial.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string firstName, middleName, lastName;
+
+    cout << "Enter your first name: ";
+    cin >> firstName;
+
+    cout << "Enter your middle name : ";
+    cin >> middleName;
+
+    cout << "Enter your last name: ";
+    cin >> lastName;
+
+    // Extract first character of each name
+    char firstInitial = firstName[0];
+    char middleInitial = middleName[0];
+    char lastInitial = lastName[0];
+
+    cout << "Your initials are: " << firstInitial << middleInitial << lastInitial << endl;
+
+    return 0;
+}
 
