83e02201ec4410aed1bdd5091d93bdf173794fc1
Nardos Bisrat <149002589+nardi-bis@users.noreply.github.com>
2024-01-14 22:38:22 +0300

    Update 2.palindrome checker.cpp

--- ETS1026_15_Nardos_Bisrat/Activity 4.2/String/2.palindrome checker.cpp
+++ ETS1026_15_Nardos_Bisrat/Activity 4.2/String/2.palindrome checker.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+
+    char word[] = "keek";
+    int j=3;
+
+    for(int i=0; i<4; i++){
+        if(word[i] != word[j]){
+            cout << "The word is not palindrome.\n";
+            return 0;
+        }
+        j--;
+    }
+    cout << "The word is a palindrome";
+    
+    return 0;
+}
