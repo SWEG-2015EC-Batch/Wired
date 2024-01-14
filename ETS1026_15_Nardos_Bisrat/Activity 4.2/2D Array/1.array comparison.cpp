a4d70cfad3ac825998ca21d8118fcc9cadcb9c27
Nardos Bisrat <149002589+nardi-bis@users.noreply.github.com>
2024-01-14 18:49:01 +0300

    Update 1.array comparison.cpp

--- ETS1026_15_Nardos_Bisrat/Activity 4.2/2D Array/1.array comparison.cpp
+++ ETS1026_15_Nardos_Bisrat/Activity 4.2/2D Array/1.array comparison.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
 
+using namespace std;
+
+int main() {
+    int size;
+
+    cout << "Enter the size of the arrays: ";
+    cin >> size;
+
+    int array1[size], array2[size];
+
+    cout << "Enter elements for the first array:\n";
+    for (int i = 0; i < size; ++i) {
+        cin >> array1[i];
+    }
+
+    cout << "Enter elements for the second array:\n";
+    for (int i = 0; i < size; ++i) {
+        cin >> array2[i];
+    }
+
+    // Check if arrays are identical
+    bool identical = true;
+    for (int i = 0; i < size; ++i) {
+        if (array1[i] != array2[i]) {
+            identical = false;
+            break;
+        }
+    }
+
+   if (identical) {
+    cout << "The arrays are identical." << endl;
+} else {
+    cout << "The arrays are not identical." << endl;
+}
+
+
+    return 0;
+}
