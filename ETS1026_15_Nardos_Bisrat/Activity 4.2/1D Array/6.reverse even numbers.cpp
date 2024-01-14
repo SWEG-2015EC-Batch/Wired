22422ffc600e37efa5e8099b455f991d31a137b0
Nardos Bisrat <149002589+nardi-bis@users.noreply.github.com>
2024-01-14 13:07:38 +0300

    Update 6.reverse even numbers.cpp

--- ETS1026_15_Nardos_Bisrat/Activity 4.2/1D Array/6.reverse even numbers.cpp
+++ ETS1026_15_Nardos_Bisrat/Activity 4.2/1D Array/6.reverse even numbers.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   int size, num;
+   const int MAX_SIZE = 100;  // Set a maximum size for the array
+   int evenNumbers[MAX_SIZE];
+   int evenCount = 0;
+
+   cout << "Enter number of elements (up to " << MAX_SIZE << "): ";
+   cin >> size;
+
+   for (int i = 0; i < size; ++i) {
+       cout << "Enter element " << (i + 1) << ": ";
+       cin >> num;
+       if (num % 2 == 0) {
+           evenNumbers[evenCount++] = num;  // Store even numbers in the array
+       }
+   }
+
+   cout << "Even numbers in reverse order: ";
+   for (int i = evenCount - 1; i >= 0; --i) {
+       cout << evenNumbers[i] <<" ";
+   }
+
+   cout << endl;
+
+   return 0;
+}
 
