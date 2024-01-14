4ec7580b17f2eaaed4e20402f2001034e81e946d
Natanim Teshome <118844475+NatanimT@users.noreply.github.com>
2024-01-14 20:12:43 +0300

    Question2.cpp

--- ETS1034_15_Natanim_Teshome/Activity4.2/1D_Array/Question2.cpp
+++ ETS1034_15_Natanim_Teshome/Activity4.2/1D_Array/Question2.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <string.h>
+#include <iomanip>
+using namespace std;
+int main(){
+    int number[8],sum(0);
+    for(int i=0;i<8;i++){
+        cout << "enter number"<<i+1<<" : ";
+        cin >> number[i];
+        sum+=number[i];
+    }
+    cout << "\nThe numbers are: "; 
+      for(int i=0;i<8;i++){
+        cout << number[i]<< setw(10);
+    }
+    double average = sum/8.0;
+    cout <<"sum: "<<sum<<"\nAverage: "<<average;
+}
 
