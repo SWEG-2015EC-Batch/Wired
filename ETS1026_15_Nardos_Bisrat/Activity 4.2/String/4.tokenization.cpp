95b0e4076dd2d0f582657b1c5c68e076d6049b29
Nardos Bisrat <149002589+nardi-bis@users.noreply.github.com>
2024-01-14 23:01:33 +0300

    Update 4.tokenization.cpp

--- ETS1026_15_Nardos_Bisrat/Activity 4.2/String/4.tokenization.cpp
+++ ETS1026_15_Nardos_Bisrat/Activity 4.2/String/4.tokenization.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+  
+  string text;
+    cout << "Enter a line of text: ";
+    getline(cin, text);
+
+    cout << "Original: " << text << '\n';
+    int count = 1;
+    cout << "Token "<< count << ": ";
+
+    for (int i = 0; i < text.size(); i++)
+    {
+        if(text[i] != ' '){
+            cout << text[i] ;
+        }else{
+            cout << "\nToken " << ++count << ": " ;
+        }
+    }
+  
+
+    return 0;
+}
+    
