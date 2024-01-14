16614c571501359a8ac0f330133993d16e09c620
Nardos Bisrat <149002589+nardi-bis@users.noreply.github.com>
2024-01-14 21:16:03 +0300

    Update 4.even and odd.cpp

--- ETS1026_15_Nardos_Bisrat/Activity 4.2/2D Array/4.even and odd.cpp
+++ ETS1026_15_Nardos_Bisrat/Activity 4.2/2D Array/4.even and odd.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    const int rows = 3, cols = 4;
+    int matrix[rows][cols];
+    int sumEven = 0;
+ cout << "Enter the elements of the 3x4 matrix:\n";
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    
+    cout << "\nModified Matrix:\n";
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            if (matrix[i][j] % 2 == 0) {
+                sumEven += matrix[i][j];
+                cout << matrix[i][j] << "\t"; 
+            } else {
+                cout << "*" << "\t";         
+            }
+        }
+        cout << endl;
+    }
+
+    cout << "\nSum of even elements: " << sumEven << endl;
+
+    return 0;
+}
+
+
 
