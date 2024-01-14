debb04a45075eeddb8cf7f115f101c34ce592d6f
Nardos Bisrat <149002589+nardi-bis@users.noreply.github.com>
2024-01-14 12:21:30 +0300

    Update 3.Array addition.cpp

--- ETS1026_15_Nardos_Bisrat/Activity 4.2/1D Array/3.Array addition.cpp
+++ ETS1026_15_Nardos_Bisrat/Activity 4.2/1D Array/3.Array addition.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int rows;
+    int cols;
+    cout<<"enter the number of rows:";
+    cin>>rows;
+    cout<<"enter the number of columns:";
+    cin>>cols;
+    int first[rows * cols], second[rows * cols], result[rows * cols];
+
+    // Read elements of first array
+    cout << "Enter elements of first array :" << endl;
+    for (int i = 0; i < rows * cols; ++i) {
+        cin >> first[i];
+    }
+
+    // Read elements of second array
+    cout << "Enter elements of second array :" << endl;
+    for (int i = 0; i < rows * cols; ++i) {
+        cin >> second[i];
+    }
+
+    // Add corresponding elements and store in result array
+    for (int i = 0; i < rows * cols; ++i) {
+
+        result[i] = first[i] + second[i];
+    }
+
+    // Print the resulting data elements in table form
+    cout << "\nResulting elements in table form:" << endl;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            int index = i * cols + j; // Calculate 1D index from 2D coordinates
+            cout << result[index] << "\t";
+        }
+        cout << endl;
+    }
+    return 0;}
 
