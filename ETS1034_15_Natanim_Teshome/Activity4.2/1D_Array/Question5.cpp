b8b98e4babf6910cd91da295bc5030544a3c571a
Natanim Teshome <118844475+NatanimT@users.noreply.github.com>
2024-01-14 20:18:19 +0300

    Question5.cpp

--- ETS1034_15_Natanim_Teshome/Activity4.2/1D_Array/Question5.cpp
+++ ETS1034_15_Natanim_Teshome/Activity4.2/1D_Array/Question5.cpp
@@ -1,31 +1,19 @@
 #include <iostream>
-#include <string.h>
-#include <iomanip>
+
 using namespace std;
-int main(){
-cout << "Enter the number of student: ";
-int n;
-cin>>n;
-int score[n];
-string scoreStatus[n];
-for(int i=0;i<n;i++){
-    cout << "enter the score of student "<<i+1<<" : ";
-    cin >> score[i]; 
-}
-for(int i=0;i<n;i++){
-if(score[i]>=80)
-scoreStatus[i] = "Excellent";
-else if(score[i]>=60) 
-scoreStatus[i] = "Very good";
-else if(score[i]>=50) 
-scoreStatus[i] = "Fair";
-else if(score[i]>=40) 
-scoreStatus[i] = "Poor";
-else
-scoreStatus[i] = "Fail";
-                    }
-cout << setw(5)<<"stud-number"<<setw(15)<< "Status "<<endl;
-for(int i=0;i<n;i++){
-cout << setw(5)<<"student-"<<i+1<<setw(15)<< scoreStatus[i]<<endl;
-}
+int main()
+{
+    int const size = 9;
+
+    double volts[size] = {11.95, 16.32, 12.15, 8.22, 15.98,
+                          26.22, 13.54, 6.45, 17.59};
+
+    for (int j = 0; j < size; j++)
+    {
+        cout << volts[j] << " ";
+        if ((j + 1) % 3 == 0)
+        {
+            cout << endl;
+        }
+    }
 }
