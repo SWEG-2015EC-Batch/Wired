d94dbbfff0306442571b242900770635663cc1a0
Nardos Bisrat <149002589+nardi-bis@users.noreply.github.com>
2024-01-14 11:19:27 +0300

    Update 2.Sum and Average.cpp

--- ETS1026_15_Nardos_Bisrat/Activity 4.2/1D Array/2.Sum and Average.cpp
+++ ETS1026_15_Nardos_Bisrat/Activity 4.2/1D Array/2.Sum and Average.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int temp [8];
+    double total=0;
+
+    for(int i=0;i<8;i++){
+        cout<<"enter number "<<i+1<<":";
+        cin>>temp[i];
+        total+=temp[i];
+    }
+        cout<<"average = "<<total/8<<endl;
+
+
+    return 0;
+}
 
