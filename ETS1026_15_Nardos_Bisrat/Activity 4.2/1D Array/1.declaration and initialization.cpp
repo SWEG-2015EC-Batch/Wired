37df39eebfc1b8f2970d9ad5a0f90c9fef04d4fd
Nardos Bisrat <149002589+nardi-bis@users.noreply.github.com>
2024-01-14 09:45:39 +0300

    Update 1.declaration and initialization.cpp

--- ETS1026_15_Nardos_Bisrat/Activity 4.2/1D Array/1.declaration and initialization.cpp
+++ ETS1026_15_Nardos_Bisrat/Activity 4.2/1D Array/1.declaration and initialization.cpp
@@ -1,1 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Declaration of 1D array with 20 elements to hold the 4x5 data
+    int arr[20] = {16, 22, 99, 4, 18, -258, 4, 101, 5, 98, 105, 6, 15, 2, 45, 33, 88, 72, 16, 3};
+    return 0;
+}
 
