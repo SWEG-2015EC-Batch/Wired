20cc32056881bddcc067a282253b452d6d29e84d
Nardos Bisrat <149002589+nardi-bis@users.noreply.github.com>
2024-01-14 22:58:40 +0300

    Update 3.non alphabet.cpp

--- ETS1026_15_Nardos_Bisrat/Activity 4.2/String/3.non alphabet.cpp
+++ ETS1026_15_Nardos_Bisrat/Activity 4.2/String/3.non alphabet.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string originalString;
+    string alphabetOnlyString;
+
+    cout << "Enter a string: ";
+    getline(cin, originalString);  // Use getline to capture spaces as well
+   
+    for (char c : originalString) {
+        if (isalpha(c)) {  
+            alphabetOnlyString += c;
+        }
+    }
+    cout << "String with only alphabets: " << alphabetOnlyString << endl;
+
+    return 0;
+}
 
