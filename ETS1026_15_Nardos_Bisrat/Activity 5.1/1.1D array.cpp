56c188a5a1b7d5cf6578f7be32440764aa990d06
Nardos Bisrat <149002589+nardi-bis@users.noreply.github.com>
2024-01-23 20:53:58 +0300

    Update 1.1D array.cpp\

--- ETS1026_15_Nardos_Bisrat/Activity 5.1/1.1D array.cpp
+++ ETS1026_15_Nardos_Bisrat/Activity 5.1/1.1D array.cpp
@@ -1,1 +1,20 @@
-
+#include <iostream>
+using namespace std;
+int main(){
+    double balance[5]={1000.0,2.0,3.4,17.0,50.0};
+    int arr[3][4]={{10,11,12,13},{20,21,22,23},{30,31,32,33}};
+    for(int i=0;i<5;i++){
+        cout << balance+i<<"  "<<endl; 
+    }
+     for(int i=0;i<12;i++){
+        cout << arr+i<<"  "<<endl; 
+    }
+    double *ptrBalance = &balance[0];
+     for(int i=0;i<5;i++){
+        cout << ptrBalance+i<<"  "<<endl; 
+    }
+    int *ptrarr= &arr[0][0];
+    for(int i=0;i<12;i++){
+        cout << ptrarr+i<<"  "<<endl; 
+    }
+}
