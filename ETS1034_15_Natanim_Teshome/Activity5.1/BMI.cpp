170ade39cd54cb63d6b107532742b2ac7f64a03e
Natanim Teshome <118844475+NatanimT@users.noreply.github.com>
2024-01-23 20:01:57 +0300

    BMI.cpp

--- ETS1034_15_Natanim_Teshome/Activity5.1/BMI.cpp
+++ ETS1034_15_Natanim_Teshome/Activity5.1/BMI.cpp
@@ -1,1 +1,20 @@
-
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main(){
+    int n;
+    cout << "For how many person do you want to make: ";
+    cin>> n;
+    double balance[n][3];
+    double *ptrbalance = &balance[0][0];
+    for(int i=0;i<n;i++){
+        cout << "Enter Weigth: ";
+        cin >> *(ptrbalance + 3*i);
+        cout << "Enter Heigth: ";
+        cin >> *(ptrbalance + 3*i + 1);
+        *(ptrbalance + 3*i + 2) = *(ptrbalance + 3*i) /(*(ptrbalance + 3*i + 1)**(ptrbalance + 3*i + 1));
+    }
+    for(int i=0;i<n;i++){
+        cout << i+1<<setw(15)<<*(ptrbalance + 3*i + 2)<<endl;
+    }
+}
